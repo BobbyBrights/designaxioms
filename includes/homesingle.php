07938bf6e714e098b015deb9f002beea74f3ff28
Jennifer Patel <jpatel@inkyblakk.com>
2012-09-18 15:25:14 -0700

    Revert "beginning to make home single changes to fix author problems"
    This reverts commit d9c801d57fbfe7a1ca7a9c4763c3f7b23cf42eb1.

--- includes/homesingle.php
+++ includes/homesingle.php
@@ -1,4 +1,4 @@
-<div id="content" class="sixteen columns singleContent">
+		<div id="content" class="sixteen columns homeContent axiomContent">
 			<ul id="slider">
 		<?php
 			$allQuoteLinks  = array();
@@ -31,47 +31,50 @@
 						$allQuoteImgs[] = $quoteImg;
 						$allQuoteAlts[] = $quoteAlt;
 						$quoteCount++;
-					}//end loop<a id="example8" href="./example/8_b.jpg" title="&lt;a href=&#34;#&#34;&gt;Sed vel sapien vel sem tempus placerat eu ut tortor. Nulla facilisi. Sed adipiscing, turpis ut cursus molestie, sem eros viverra mauris, quis sollicitudin sapien enim nec est. ras pulvinar placerat diam eu consectetur.&lt;/a&gt;"><img class="last" alt="example7" src="./example/8_s.jpg" /></a>
+					}//end loop
 				
-				echo"		<li class=\"panel\">\n";
+				echo"		<li class=\"panel\">";
 					if ($cardType == 'author'){
-						echo"					<div class=\"authorItem slide\" id=\"" . $imageId . "\">\n
-						<div class=\"authorFlag\"></div>";
+						echo"<div class=\"authorItem slide\" id=\"" . $imageId . "\">
+						<a class=\"hires\" href=\"" . $hiRes . "\" title=\"&lt;a href=&#34;" . $hiRes . "&#34;&gt;Download this card&lt;/a&gt;\"><img src=\"" . $image . "\" class=\"authorPortrait\" alt=\"" . $title . "\" width=\"288\" height=\"432\" /></a>";
 					} else {
-						echo "					<div class=\"axiomItem slide\" id=\"" . $imageId . "\">\n";
+						echo "<div class=\"axiomItem slide\" id=\"" . $imageId . "\">
+						<a class=\"hires\" href=\"" . $hiRes . "\" title=\"&lt;a href=&#34;" . $hiRes . "&#34; target=&#34;blank&#34; &gt;Download this card&lt;/a&gt;\"><img src=\"" . $image . "\" alt=\"" . $title . "\" width=\"288\" height=\"432\" /></a>"; 
+					
 					}
-					echo "						<a class=\"hires\" href=\"" . $hiRes . "\" title=\"<a href=" . $hiRes . "&#34; target=&#34;blank&#34;>Download this card</a>\"><img src=\"" . $image . "\" alt=\"" . $title . "\" width=\"288\" height=\"432\" /></a> 
-						<div class=\"axiomInfo\">\n";
+						echo "<div class=\"axiomInfo\">";
 						if($cardType == 'quote'){
-							echo"							<div class=\"quoteFlag\"></div>\n";
+							echo"<div class=\"quoteFlag\"></div> ";
 						}else if($cardType == 'axiom'){
-							echo"							<div class=\"axiomFlag\"></div>\n";
+							echo"<div class=\"axiomFlag\"></div> ";
+						}else if($cardType == 'author'){
+							echo "<div class=\"authorFlag\"></div>";
 						}
-						echo"							<h1>" . $title . "</h1>"; 
+						echo"<h1>" . $title . "</h1>"; 
 						if ($cardType == 'author'){
 							echo "<h5>" . $subtitle . "</h5>";
 						} else if ($cardType == 'quote'){
 							echo "<h4><a href=\"" . $authorLink . "\">" . $subtitle . "</a></h4>";
 						}
-						echo "<hr>" . $description . " 
-						</div>\n"; 
-						/*if ($cardType == 'author') {
-							echo"						<div class=\"quotedCards\">
+						echo "<hr>
+															" . $description . " 
+					</div>"; 
+						if ($cardType == 'author') {
+							echo"<div class=\"quotedCards\">
 							<ul>\n";
 							
 							for($i=0;$i<$quoteCount;$i++) {
 								//you should add the alts to the xml file as attributes
-								echo "								<li><a href=\"" . $allQuoteLinks[$i] . "\"><img src=\"" . $allQuoteImgs[$i] . "\" alt=\"" . $allQuoteAlts[$i] . "\" width=\"69px\" height=\"103px\" /></a></li>\n";
+								echo "								<li><a href=\"" . $allQuoteLinks[$i] . "\"><img src=\"" . $allQuoteImgs[$i] . "\" alt=\"" . $allQuoteAlts[$i] . "\" width=\"69\" height=\"98\" /></a></li>\n";
 							}//end loop			
 								echo	"							</ul>
-						</div>\n";
-						}*/
+									</div>	";
+						}
 					
-				echo"					</div> 
-				</li> \n		";
+				echo"</div> 
+			</li> \n		";
 
 			}		
 		?>
-	</ul><!--end #slider1-->
+			</ul><!--end #slider1-->
 		</div><!--end #content-->
-		
